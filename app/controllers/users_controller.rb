9d2a825de8c91f081cbea2a7ee6d5a192c024b36
jpv2018 <jason.viola@mavs.uta.edu>
2020-07-11 14:31:06 -0500

    posts to /users and adds redirects

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,37 +1,18 @@
 class UsersController < ApplicationController
 
-  # GET: /users
-  get "/users" do
-    erb :"/users/index.html"
+  get '/login' do
+    erb :login
   end
 
-  # GET: /users/new
-  get "/users/new" do
-    erb :"/users/new.html"
+  get '/register' do
+    erb :register
   end
 
-  # POST: /users
-  post "/users" do
-    redirect "/users"
-  end
-
-  # GET: /users/5
-  get "/users/:id" do
-    erb :"/users/show.html"
-  end
-
-  # GET: /users/5/edit
-  get "/users/:id/edit" do
-    erb :"/users/edit.html"
-  end
-
-  # PATCH: /users/5
-  patch "/users/:id" do
-    redirect "/users/:id"
-  end
-
-  # DELETE: /users/5/delete
-  delete "/users/:id/delete" do
-    redirect "/users"
-  end
+  post '/users' do
+    @user = User.new(params)
+    if @user.save
+      redirect to "/users/#{@user.id}"
+    else
+      redirect to '/register'
+    end
 end
