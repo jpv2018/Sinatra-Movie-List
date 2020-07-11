c76f98aca5a7af06603784411fc3763fbf0c4a92
jpv2018 <jason.viola@mavs.uta.edu>
2020-07-10 21:50:42 -0500

    initial project setup via corneal

---
+++ db/migrate/20200711024452_create_movies.rb
@@ -0,0 +1,9 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :user_id
+      t.timestamps null: false
+    end
+  end
+end
